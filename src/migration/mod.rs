ad5e5e20ad180d110d74cb80d2e76cc60b281803
Yogesh Surendran <carreraphp@gmail.com>
2022-12-01 01:54:57 +0530

    feat(migrate): add import alias enum for language support

--- src/migration/mod.rs
+++ src/migration/mod.rs
@@ -1,4 +1,5 @@
-
+use std::path::PathBuf;
+use serde_json::{Value};
 
 #[derive(Default, Debug, Clone)]
 pub(crate) enum MigrationType {
@@ -6,4 +7,11 @@ pub(crate) enum MigrationType {
     EmotionToScss,
     LegacyToLatest,
     // None,
+}
+
+#[derive(Debug, Clone)]
+pub(crate) enum MigrationImportAliasType {
+    TypeScript(Value, PathBuf),
+    JavaScript(Value, PathBuf),
+    None(PathBuf),
 }
