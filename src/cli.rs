df8bdf6840c4e1448734286f2b163546121c5733
Yogesh Surendran <carreraphp@gmail.com>
2022-12-01 02:06:30 +0530

    feat(cli): create clap argument for js_alias

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,5 @@
+use clap::{Arg, ArgAction};
+
 
 use crate::migration::{
     MigrationOptions,
@@ -14,4 +16,13 @@ pub(crate) enum CliOptions {
     Migration(MigrationOptions),
     Error(String),
     None,
+}
+
+fn create_js_alias_argument() -> Arg {
+    Arg::new("js_alias")
+        .short('A')
+        .long("js-config-alias")
+        .action(ArgAction::SetTrue)
+        .help("is js/ts config aliases enabled in the repo?")
+        .value_name("BOOLEAN")
 }
